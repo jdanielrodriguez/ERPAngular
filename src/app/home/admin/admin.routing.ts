0e955b2e567a184dffd1b679e8dc248b80e29ca2
Daniel Rodriguez <dannyjose1112@hotmail.com>
2017-12-05 16:38:37 -0600

    [UPDATED] added config for nav

--- src/app/home/admin/admin.routing.ts
+++ src/app/home/admin/admin.routing.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { AdminComponent } from "./admin.component";
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '', component: AdminComponent, children: [
     { path: 'dashboard', component: DashboardComponent },
-    // { path: 'alumnos/:op', component: AlumnosComponent },
+    { path: 'usuarios', component: UsuariosComponent },
+    { path: 'empleados', component: EmpleadosComponent },
     // { path: 'cursos/:op/:name', component: CursosAlumnoComponent },
     // { path: 'tareas/:id/:name', component: TareasCursoComponent },
     // { path: 'cobros/:id/:name', component: CobrosComponent },
