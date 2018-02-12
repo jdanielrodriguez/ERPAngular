08bca1673eb56d1bc27092a51e4e90319ca72739
Daniel Rodriguez <dannyjose1112@hotmail.com>
2018-02-12 15:08:25 -0600

    [UDPATED] added dashboard charts

--- src/app/home/admin/dashboard/dashboard.component.ts
+++ src/app/home/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { EstadisticasService } from "./../_services/estadisticas.service";
+
+import { NotificationsService } from 'angular2-notifications';
+
+declare var $: any
 
 @Component({
   selector: 'app-dashboard',
@@ -6,50 +11,193 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-   // lineChart
-   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
+  title:string="Dashboard"
+  Table:any
+  idRol=+localStorage.getItem('currentRolId');
+  Agregar = +localStorage.getItem('permisoAgregar')
+  Modificar = +localStorage.getItem('permisoModificar')
+  Eliminar = +localStorage.getItem('permisoEliminar')
+  Mostrar = +localStorage.getItem('permisoMostrar')
+  selectedData:any
+  public rowsOnPage = 5;
+  public search:any
+  fechaHoy:any
+  fechaMes:any
+  constructor(
+    private _service: NotificationsService,
+    private mainService: EstadisticasService
+  ) { }
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
     responsive: true
   };
-  public lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+
+  public barChartLabels:string[] = [];
+
+  public barChartData:any[] = [
+    {data:[],label:'Vacio'}
   ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
+
+   public pieChartLabels:any[];
+   public pieChartData:any=[];
+   public pieChartType:string = 'pie';
+
+  ngOnInit() {
+    this.getDate();
+    this.cargarPie();
+    this.cargarDias();
+    this.cargarBarra();
+    setTimeout(() => {
+      this.cargarDias();
+      this.cargarBarra();
+    }, 500);
+  }
+  cargarDias(){
+    let dias;
+    let array:any = []
+    this.barChartLabels.length = 0
+    dias = this.fechaMes
+    let fechaInicio = new Date(dias).getTime();
+    let fechaFin    = new Date(this.fechaHoy).getTime();
+    this.barChartLabels.length=0;
+    let diff = fechaFin - fechaInicio;
+    dias = (diff/(1000*60*60*24))
+    var hoy = new Date(this.fechaMes);
+    for(let i=1;i<=(dias+1);i++){
+      var calculado = new Date(this.fechaMes);
+      calculado.setDate(
+        (hoy.getDate()) + i
+      );
+      let fecha = calculado.getFullYear() + '-' +(((calculado.getMonth() + 1)<10)?'0'+(calculado.getMonth() + 1):(calculado.getMonth() + 1)) + '-' + ((calculado.getDate()<10)?'0'+calculado.getDate():calculado.getDate());
+      array.push(fecha)
+    }
+
+    this.barChartLabels = array
+
+  }
+  getDate(){
+    let date = new Date();
+    let month = date.getMonth()+1;
+    let month2;
+    let diaB = date.getDate()-1;
+    let diaB2;
+    let dia= date.getDate();
+    let dia2;
+    if(month<10){
+      month2='0'+month;
+    }else{
+      month2=month
+    }
+    if(diaB<10){
+      diaB2='0'+diaB;
+    }else{
+      diaB2=diaB
+    }
+    if(dia<10){
+      dia2='0'+dia;
+    }else{
+      dia2=dia
+    }
+    this.fechaHoy= date.getFullYear()+'-'+month2+'-'+dia2
+    this.fechaMes= date.getFullYear()+'-'+month2+'-'+diaB2
+  }
+  cargarBarra(){
+    $('#Loading').css('display','block')
+    $('#Loading').addClass('in')
+    let data = {
+      fechaInicio: this.fechaMes,
+      fechaFin: this.fechaHoy
+    }
+    this.mainService.getBarVentas(data)
+                      .then(response => {
+                        if(response){
+                          let fortex:any = []
+                          fortex.length = 0
+                          response.forEach((element,index) => {
+                              let dat = {
+                                label:'Ventas',
+                                data:[]
+                              }
+                              let bandera = 0
+                              this.barChartLabels.forEach((element2,x) => {
+                                if(element2==element.fecha){
+                                  if(fortex.find((element1,i) => {
+                                     return element1.label=='Ventas'
+                                  })){
+                                    fortex[fortex.findIndex((element1,i) => {
+                                      return element1.label=='Ventas'
+                                   })].data[x]=element.total
+                                        bandera=1
+                                  }else{
+                                    dat.data.push(element.total)
+
+                                  }
+                                }else{
+                                  dat.data.push(0)
+                                }
+                              })
+                              if(bandera==0){
+                                fortex.push(dat)
+                              }
+
+                          });
+                          if(fortex.length>0){
+                            this.barChartData = fortex;
+                          }
+
+                        }
+                        // console.log(this.barChartLabels);
+                        // console.log(this.barChartData)
+
+                        $('#Loading').css('display','none')
+                        console.clear
+                      }).catch(error => {
+                        console.clear
+                        this.createError(error)
+                        $('#Loading').css('display','none')
+                      })
+  }
+
+  cargarPie(){
+    $('#Loading').css('display','block')
+    $('#Loading').addClass('in')
+    let data = {
+      fechaInicio: this.fechaMes,
+      fechaFin: this.fechaHoy
+    }
+    this.pieChartData = null
+    this.pieChartLabels = null
+    this.mainService.getPieVentas(data)
+                      .then(response => {
+                          let labels:any[] = []
+                          let data:any[] = []
+                          response.forEach((element,index) => {
+                            labels.push(element.nombre)
+                            data.push(element.total)
+
+                          });
+                          // if(fortex.length>0 && fortexData.length>0){
+                          //   this.pieChartLabels = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+                          //   this.pieChartData = [300, 500, 100];
+                          // }else{
+                          //   this.pieChartData = [0]
+                          //   this.pieChartLabels = ["Vacio"]
+                          // }
+                          this.pieChartLabels=labels
+                          this.pieChartData=data
+                        // console.log(this.pieChartLabels);
+                        // console.log(this.pieChartData)
+
+                        $('#Loading').css('display','none')
+                        console.clear
+                      }).catch(error => {
+                        console.clear
+                        this.createError(error)
+                        $('#Loading').css('display','none')
+                      })
+  }
 
   public chartClicked(e:any):void {
     console.log(e);
@@ -58,38 +206,24 @@ export class DashboardComponent implements OnInit {
   public chartHovered(e:any):void {
     console.log(e);
   }
-  public barChartOptions:any = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
 
-  public barChartData:any[] = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
+  public options = {
+    position: ["bottom", "right"],
+    timeOut: 2000,
+    lastOnBottom: false,
+    animate: "fromLeft",
+    showProgressBar: false,
+    pauseOnHover: true,
+    clickToClose: true,
+    maxLength: 200
+};
 
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
-  public doughnutChartType:string = 'doughnut';
+create(success) {
+     this._service.success('¡Éxito!',success)
 
-  public radarChartLabels:string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+}
+createError(error) {
+     this._service.error('¡Error!',error)
 
-   public radarChartData:any = [
-     {data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A'},
-     {data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B'}
-   ];
-   public radarChartType:string = 'radar';
-
-   public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
-   public pieChartData:number[] = [300, 500, 100];
-   public pieChartType:string = 'pie';
-
-   public polarAreaChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
-   public polarAreaChartData:number[] = [300, 500, 100, 40, 120];
-   public polarAreaLegend:boolean = true;
-
-   public polarAreaChartType:string = 'polarArea';
+}
 }
